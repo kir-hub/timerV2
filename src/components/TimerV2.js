3ba91a42375fcf34dfcf0aa86e87cf5eab162014
kir-hub <kirill.silin.12321@gmail.com>
2021-03-16 17:58:41 +0200

    try to made const with default value with event and fixed arrowFunctions and removed spaces

--- src/components/TimerV2.js
+++ src/components/TimerV2.js
@@ -1,39 +1,20 @@
-import React, {useState, useCallback, useRef, useEffect} from 'react'
+import React, {useState,  useRef, useEffect} from 'react'
 
 export default function TimerV2(props) {
     const {hour, minute, second} = props
-    
     const [timer, setTimer] = useState({s: second, m: minute, h: hour})
-
-
     const calculateTimer = ()=>{
-        
-        
-        
         const sec = (timer.h == 0 && timer.m == 0 && timer.s == 0) ?  0 : ((timer.s == 0 && timer.m > 0) ? 5 : timer.s - 1 )
-
-        const min = (timer.h == 0 && timer.m == 0) ? 0 : ((timer.h > 0 && timer.m == 0 ) ? 5 : (timer.m > 0 && timer.s == 0 ? timer.m - 1 : timer.m )) //timer.m - 1)  && timer.m == 0  && timer.s == 0) ? 5 : timer.m ) ? timer.m - 1  //(( ((sec == 0 && timer.h > 0) && 5) ?  timer.m - 1 : timer.m))
-        
+        const min = (timer.h == 0 && timer.m == 0) ? 0 : ((timer.h > 0 && timer.m == 0 ) ? 5 : (timer.m > 0 && timer.s == 0 ? timer.m - 1 : timer.m )) 
         const hours = (timer.h == 0) ? 0  : (timer.m == 0 && timer.h > 0)  ? timer.h - 1  : timer.h
-        
-        return {
-            s: sec,
-            m: min,
-            h: hours
-        };
+        return { s: sec,m: min,h: hours};
     }
-
     const formatValues = (timer) => ({
         s: timer.s < 10 && '0' + timer.s,
         m: timer.m < 10 && '0' + timer.m,
         h: timer.h < 10 && '0' + timer.h
     })
-
-    useEffect(() =>{
-        calculateTimer()
-        
-    }, []);
-
+    useEffect(() => calculateTimer() ,[]);
     useEffect(() => {
         const timeout = setTimeout(() => {
             setTimer(calculateTimer());
